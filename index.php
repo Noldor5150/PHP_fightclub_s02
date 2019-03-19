1c1c2bdb972f5facf4cbc2b8a1303d8239d08b51
Noldor5150 <ruikis.paulius@gmail.com>
2019-03-19 12:14:47 +0200

    trecia uzduotis oop

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
+
 class ThailandSurpise {
+
     public $clothes;
     private $balls;
 
-
-
-public function __const() {
+    public function __const() {
         $this->balls = rand(0, 1);
     }
+
+    public function attachBalls() {
+        $this->balls = true;
+    }
+    public function detachBalls() {
+        $this->balls = false;
+    }
+
+
 }
