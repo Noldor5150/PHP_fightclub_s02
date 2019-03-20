c6c9847b1040a79654608304d563ac251667f594
Noldor5150 <ruikis.paulius@gmail.com>
2019-03-20 13:04:23 +0200

    oop su dependency injection ir su set get 3uzduotis

--- index.php
+++ index.php
@@ -1,29 +1,60 @@
 <?php
+declare (strict_types = 1);
 
-Class Jacuzzi {
+Class Gerimas {
 
-    public $amount_water;
-    public $amount_non_water;
+    private $data;
 
-    public function __construct($wasser = 0, $nicht_wasser = 0) {
-        $this->amount_water = $wasser;
-        $this->amount_non_water = $nicht_wasser;
+    public function __construct() {
+        $this->data = ['name' => null,
+            '$amount_ml' => null,
+            ' $abarot' => null];
     }
 
-    public function getWaterPurity() {
+    public function setName(string $name) {
+        $this->name = $name;
+    }
 
-        return $this->amount_water / ($this->amount_water + $this->amount_non_water) * 100;
+    public function setAmount(int $amount_ml) {
+        $this->amount_ml = $amount_ml;
+    }
+
+    public function setAbarot(int $abarot) {
+        $this->abarot = $abarot;
+    }
+
+    public function getName() {
+        return $this->data['name'];
+    }
+
+    public function getAmount() {
+        return $this->data['amount_ml'];
+    }
+
+    public function getAbarot() {
+        return $this->data['abarot'];
+    }
+    
+      public function getData(){
+        return $this->data;
+    }
+  
+     public function setData($data){
+        return $this->data = [
+            'name' => setName($data['name']),
+            'amount_ml' => setAmount($data['amount_mle']),
+            'abarot' => setAbarot($data['abarot'])
+        ];
     }
 
 }
-class User{
-    public function peeInJakuzzi(Jacuzzi $jakuzzi,$amount) {
-        $jakuzzi->amount_non_water +=$amount ;
-    }
-}
-$maudynes = new Jacuzzi(600000,0);
-$Piotras = new User ;
-$Piotra = new User ;
-$Piotras->peeInJakuzzi($maudynes, rand(1000, 2000));
-$Piotra->peeInJakuzzi($maudynes, rand(500, 1000));
-print $maudynes->getWaterPurity();
+$gerimas = new Gerimas();
+?>
+<html>
+    <head>
+        <title>OOP set ir get</title>
+    </head>
+    <body>
+
+    </body>
+</html>
