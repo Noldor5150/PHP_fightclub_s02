84f1955567918f6c79e0efe2a9afe9be14020304
Noldor5150 <ruikis.paulius@gmail.com>
2019-03-21 12:25:59 +0200

    failu struktura nuajam projektui

---
+++ bootloader.php
@@ -0,0 +1,4 @@
+<?php
+define('ROOT_DIR', __DIR__);
+require ROOT_DIR . '/core/functions/form.php';
+require ROOT_DIR . '/vendor/autoload.php';
--- public_html/index.php
+++ public_html/index.php
@@ -1,65 +1,8 @@
 <?php
-declare (strict_types = 1);
 
-Class Gerimas {
+require '../bootloader.php';
 
-    private $data;
-
-    public function __construct() {
-        $this->data = ['name' => null,
-            '$amount_ml' => null,
-            ' $abarot' => null];
-    }
-
-    public function setName(string $name) {
-        $this->name = $name;
-    }
-
-    public function setAmount(int $amount_ml) {
-        $this->amount_ml = $amount_ml;
-    }
-
-    public function setAbarot(float $abarot) {
-        $this->abarot = $abarot;
-    }
-
-    public function getName() {
-        return $this->data['name'];
-    }
-
-    public function getAmount() {
-        return $this->data['amount_ml'];
-    }
-
-    public function getAbarot() {
-        return $this->data['abarot'];
-    }
-    
-      public function getData(){
-        return $this->data;
-    }
-  
-     public function setData(array $data){
-        $this->setName($data['name']);
-        $this->setAmount($data['amount_ml']);
-        $this->setAbarot($data['abarot']);
-        
-    }
-
-}
-$vodke = new Gerimas();
-$vodke->setData([
-    'name' => 'Sobieskio',
-    'amount_ml' => 700,
-    'abarot' => 70.5
-]);
-var_dump($vodke);
+$db = new Core\FileDB(ROOT_DIR.'/app/files/db.txt');
+$db->save();
 ?>
-<html>
-    <head>
-        <title>OOP set ir get</title>
-    </head>
-    <body>
 
-    </body>
-</html>
