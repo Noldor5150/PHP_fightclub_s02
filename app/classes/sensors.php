b906e489eb5ac70afc23689c48abf70b1302327f
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-03 12:37:12 +0300

    sensors klases kurimas pataisom komentara

--- app/classes/sensors.php
+++ app/classes/sensors.php
@@ -2,33 +2,31 @@
 
 namespace App;
 
-class Sensors{
-    
-    /** $var Sensor[] */
+class Sensors {
+
+    /** $var App\Abstracts\Sensor[] */
     private $sensors;
-    
+
     public function __construct() {
         $this->sensors = [];
-        
     }
-
 
     public function add($id, App\Abstracts\Sensor $sensor) {
-         $this->sensors[$id] = $sensor; 
+        $this->sensors[$id] = $sensor;
     }
-    
-    public function getReading($id){
+
+    public function getReading($id) {
         return $this->sensors[$id]->read();
     }
-    
-    public function getReadings(){
-        $readings=[];
-        
-        foreach($this->sensors as $id => $sensor) {
+
+    public function getReadings() {
+        $readings = [];
+
+        foreach ($this->sensors as $id => $sensor) {
             $readings[$id] = $sensor->read();
         }
-        
+
         return $readings;
     }
+
 }
- 
