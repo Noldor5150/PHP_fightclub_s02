a4bb15dc06f3ddffea7ad6e0a279b394a74a0fd7
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-03 13:59:05 +0300

    skuriam abstrakcia klase core/abstracts/cookie.php

--- core/abstracts/cookie.php
+++ core/abstracts/cookie.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+abstract class Cookie {
 
+    protected $name;
+
+    abstract public function __construct(string $name): void;
+
+    abstract public function read(): array;
+
+    abstract public function save(array $data, int $expires_in): void;
+
+    abstract public function delete(): void;
+}
