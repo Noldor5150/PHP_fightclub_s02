881e5296016141ebe8ddd291fba67b9011b9184d
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-03 12:34:37 +0300

    sensors klases kurimas

---
+++ app/classes/sensors.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+class Sensors{
+    
+    /** $var Sensor[] */
+    private $sensors;
+    
+    public function __construct() {
+        $this->sensors = [];
+        
+    }
+
+
+    public function add($id, App\Abstracts\Sensor $sensor) {
+         $this->sensors[$id] = $sensor; 
+    }
+    
+    public function getReading($id){
+        return $this->sensors[$id]->read();
+    }
+    
+    public function getReadings(){
+        $readings=[];
+        
+        foreach($this->sensors as $id => $sensor) {
+            $readings[$id] = $sensor->read();
+        }
+        
+        return $readings;
+    }
+}
+ 
