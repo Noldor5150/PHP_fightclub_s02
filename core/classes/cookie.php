d110f6d2462851839b7a8bf3eaa398dbdfd3371a
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-04 12:05:49 +0300

    kuriam neabstracine kuki klase, kuri extendina abstrakcine

---
+++ core/classes/cookie.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+class Cookie extends Core\Abstracts\Cookie {
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function delete(): void {
+        
+    }
+
+    public function exists(): bool {
+        
+    }
+
+    public function read(): array {
+        
+    }
+
+    public function save($data, $expires_in = 3600): void {
+        
+    }
+
+}
